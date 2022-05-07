9f4853c27a0eb3152ca3bff45e459bb9dc7ea075
andreevsergey <andreev,sv89@yandex.ru>
2022-05-08 01:39:35 +0300

    add validation title description to products add rspec models spec add config should matcher to rails_helper.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {  should validate_presence_of :title  }
+  it {  should validate_presence_of :description  }
+  it 'have many attached files' do
+    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
 end
