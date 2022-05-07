294e95b00a4e5eb2a2d151ba1145389cccc589d7
andreevsergey <andreev,sv89@yandex.ru>
2022-05-08 02:02:25 +0300

    add spec for product images

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Product, type: :model do
   it {  should validate_presence_of :title  }
   it {  should validate_presence_of :description  }
   it 'have many attached files' do
-    expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+    expect(Product.new.pictures).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
 end
